6d4e9b2e00f0adce9078099c28c071ecb06a157b
Manis2025 <maniskumarchoudhury52@gmail.com>
2023-08-30 18:12:28 +0530

    my new changes

--- emart/src/App.js
+++ emart/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Nav/Navbar';
+import Gutter from './components/Gutter/Gut';
+import BrandSlide from './components/WomenComponent/BrandSlide';
 
 function App() {
   return (
     <>
     <Navbar/>
+  
+    <Gutter/>
     </>
   );
 }
