d56d508af9bd987690f7268fd652093c4e347026
Manis2025 <maniskumarchoudhury52@gmail.com>
2023-08-30 15:27:28 +0530

    gutter added

---
+++ emart/src/components/Gutter/Gut.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function Gut() {
+  return (
+    <>
+      <div class="container text-center">
+        <div class="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              BAGS
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              PRE-OWNED
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              PANTS
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              SHORTS
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              JEANS
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              BEACH WEAR
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              BOOTS
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              HATS
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              KNITWEAR
+            </div>
+          </div>
+          <div class="col">
+            <div class="p-3" style={{ border: "1px solid black" }}>
+              SUNGLASSES
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
