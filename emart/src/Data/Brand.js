7df6d2a2cf57975a33ef8d07e5b808cc291fd9b6
Liza Rani Biswal <lizaranibiswal97@gmail.com>
2023-08-30 15:07:09 +0530

    added Data

---
+++ emart/src/Data/Brand.js
@@ -0,0 +1,37 @@
+export const Brand = [
+   
+
+    {
+        src: "https://cdn.modesens.com/designer/TOM-FORD-1527073128?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/valentino-1470104215?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/puma-1470104058?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/diesel-1470103986?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/STONE-ISLAND-1489730738?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/PALM-ANGELS-1486425038?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/maison_margiela-1470104030?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/MONCLER-1509678131?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/moschino-1470104044?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/TOMMY-HILFIGER-1475948369?w=400",
+    },
+    {
+        src: "https://cdn.modesens.com/designer/kenzo-logo2.png?w=400",
+    }
+    ]
---
+++ emart/src/Data/Trend.js
@@ -0,0 +1,56 @@
+export const trenddata = [
+   
+
+    {
+        src: "https://cdn.modesens.com/product/65230877_33?w=400",
+        title: "BURBERRY",
+        sub: "Bbry carrick te Cn Ss chckd Logo SI",
+        rate: "$217 - $570",
+        st: "25 Stores"
+    },
+   
+    {
+        src: "https://is4.fwrdassets.com/images/p/fw/p/KTAH-WD90_V1.jpg",
+        title: "KHAITE",
+        sub: "Mitsi Black Padded Shell Skirt",
+        rate: "$272 - $1980",
+        st: "24 Stores"
+    },
+
+    {
+        src: "https://cdn-images.farfetch-contents.com/19/52/82/27/19528227_43527459_1000.jpg",
+        title: "JACQUEMUS",
+        sub: "La Robe Limao Cutout Halter Neck",
+        rate: "$340 - $775",
+        st: "23 Stores"
+    },
+
+    {
+        src: "https://cdn.modesens.com/product/65572195_30?w=400",
+        title: "DOLCE & GABBANA",
+        sub: "Retro Logo-Print Oversized T-shirt In",
+        rate: "$232 - $645",
+        st: "18 Stores"
+    },
+    {
+        src: "https://cdn.modesens.com/product/58881925_1?w=400",
+        title: "GANNI",
+        sub: "Women's Black Other Materials Sandals",
+        rate: "$190 - $471",
+        st: "20 Stores"
+    },
+    {
+        src: "https://cdn.modesens.com/product/71124909_25?w=400",
+        title: "BURBERRY",
+        sub: '+Net Sustain Checked Poplin-trimmed ',
+        rate: "$360 - $619",
+        st: "20 Stores"
+    },
+    {
+        src: "https://m.media-amazon.com/images/G/01/Shopbop/p/prod/products/tvamp/tvamp3006312623/tvamp3006312623_1672328798303_2-0.jpg",
+        title: "VERSACE",
+        sub: "Multicolor Trigreca Snakers In  A Mix Of",
+        rate: "$484 - $1048",
+        st: "18 Stores"
+    }
+    ]
