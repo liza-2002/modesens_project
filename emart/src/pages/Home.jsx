d9a8aef4619f0940bc11123a3bf58e2df478fa1a
Manis2025 <maniskumarchoudhury52@gmail.com>
2023-08-31 13:04:12 +0530

    commit

--- emart/src/pages/Home.jsx
+++ emart/src/pages/Home.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import Navbar from '../components/Nav/Navbar'
-import WomenFirstSlide from '../components/WomenComponent/WomenFirstSlide'
-import BrandSlide from '../components/WomenComponent/BrandSlide'
-import TrendNow from '../components/WomenComponent/TrendNow'
-import SpecialOffer from '../components/WomenComponent/SpecialOffer'
-import CommunityPost from '../components/WomenComponent/CommunityPost'
-import RoundImage from '../components/WomenComponent/RoundImage'
+import React from "react";
+import Navbar from "../components/Nav/Navbar";
+import WomenFirstSlide from "../components/WomenComponent/WomenFirstSlide";
+import BrandSlide from "../components/WomenComponent/BrandSlide";
+import TrendNow from "../components/WomenComponent/TrendNow";
+import SpecialOffer from "../components/WomenComponent/SpecialOffer";
+import CommunityPost from "../components/WomenComponent/CommunityPost";
+import RoundImage from "../components/WomenComponent/RoundImage";
+import Gut from "../components/Gutter/Gut";
+import { MenGUt } from "../components/Gutter/MenGUt";
 // import BeautyCard from '../components/Beauty/BeautyCard'
 
 export default function Home() {
   return (
     <div style={{ textAlign: "center", zIndex: "-3" }}>
-      <Navbar/>
-      <WomenFirstSlide/>
+      
+      <WomenFirstSlide />
+      <br />
+      <br />
+      <br />
+      <BrandSlide />
+      <br />
+      <RoundImage />
+      <br />
+      <TrendNow />
+      <br />
+      <Gut />
       <br/>
-      <br/>
-      <br/>
-      <BrandSlide/>
-      <br/>
-      <RoundImage/>
-      <br/>
-      <TrendNow/>
-      <br/>
-      <SpecialOffer/>
-      <br/>
-      <CommunityPost/>
+      <SpecialOffer />
+      <br />
+      <CommunityPost />
     </div>
-  )
+  );
 }
