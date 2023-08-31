4476023cd14775e5d3c0cb8a67c11153024f3b59
Liza Rani Biswal <105770467+liza-2002@users.noreply.github.com>
2023-08-31 09:03:05 +0530

    Update App.js
    modified

--- emart/src/App.js
+++ emart/src/App.js
@@ -1,16 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Nav/Navbar';
-import Gutter from './components/Gutter/Gut';
-import BrandSlide from './components/WomenComponent/BrandSlide';
-import Home from './pages/Home';
 
 function App() {
   return (
     <>
-    <Navbar/>  
-    <Gutter/>
-    <Home/>
     </>
   );
 }
